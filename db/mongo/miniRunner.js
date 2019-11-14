f40d5498e90da9051cc8fdd8e4ef377d0b941b57
Late1nAutumn <forestpu@163.com>
2019-11-13 17:09:00 -0800

    createindexes

--- db/mongo/miniRunner.js
+++ db/mongo/miniRunner.js
@@ -1,6 +1,6 @@
 const db = require("./index");
 const model = require("./models");
 var start = (new Date()).getTime();
-model.createIndex({id:1}).then(()=>{
+model.createIndexes({id:1}).then(()=>{
   console.log((new Date()).getTime()-start);
 })
