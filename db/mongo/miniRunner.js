7d21c02bc3482fb03353f853e086b029609318e7
Late1nAutumn <forestpu@163.com>
2019-11-14 09:20:26 -0800

    recover

--- db/mongo/miniRunner.js
+++
@@ -1,6 +0,0 @@
-const db = require("./index");
-const model = require("./models");
-var start = (new Date()).getTime();
-model.createIndexes({id:1}).then(()=>{
-  console.log((new Date()).getTime()-start);
-})
